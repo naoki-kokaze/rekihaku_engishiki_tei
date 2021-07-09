5465d97e1797cdb2a18870bf625d541a7b94e084
naoki kokaze <xiao3feng10324@yahoo.co.jp>
2021-07-09 21:14:31 +0900

    巻11, 14, 39の<anchor>にIDを自動付与、および巻5・17の追加

---
+++ Python/id_anchors.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup as BS
+
+vol = input('巻数は?\n')
+output_filename = f'engishiki_v{vol}.xml'
+
+f_input = open(output_filename, 'rb')
+soup = BS(f_input, 'xml')
+f_input.close()
+
+paragraphs = soup.select('p[ana]')
+
+for p in paragraphs:
+    anchors = p.select('anchor')
+    for index, anchor in enumerate(anchors):
+        anchor_id = 'app' + p["xml:id"][4:] + str((index // 2) + 1).zfill(2)
+        if index % 2 == 0:
+            anchor["xml:id"] = anchor_id
+        else:
+            anchor["xml:id"] = anchor_id + 'e'
+
+f_output = open(output_filename, 'w', encoding='utf-8')
+f_output.write(str(soup))
+f_output.close()
