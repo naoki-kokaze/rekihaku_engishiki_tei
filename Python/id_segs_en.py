e48537ad53576f27d03b4d196541a3d72fad8848
naoki kokaze <33674696+naoki-kokaze@users.noreply.github.com>
2024-07-25 15:13:47 +0900

    英訳註IDの付与対象をnoteタグとpタグに拡張

--- Python/id_segs_en.py
+++ Python/id_segs_en.py
@@ -7,13 +7,22 @@ f_input = open(f'../TEI編集用/{output_filename}', 'rb')
 soup = BS(f_input, 'xml')
 f_input.close()
 
-paragraphs = soup.select('p[ana]')
+articles = soup.select('div[type="条"]')
 
-for p in paragraphs:
-    segs = p.select('seg')
-    for index, seg in enumerate(segs):
-        seg_id = 'footnote' + p["xml:id"][4:] + str(index + 1).zfill(2)
-        seg["xml:id"] = seg_id
+for article in articles:
+    note_tag = article.find('note')
+    p_tag = article.find('p')    
+    
+    note_segs = note_tag.select('seg')
+    p_segs = p_tag.select('seg')
+
+    for index, note_seg in enumerate(note_segs):
+        note_seg_id = 'footnote' + article["xml:id"][7:] + '00' + str(index + 1).zfill(2)
+        note_seg["xml:id"] = note_seg_id
+
+    for index, p_seg in enumerate(p_segs):
+        p_seg_id = 'footnote' + p_tag["xml:id"][4:] + str(index + 1).zfill(2)
+        p_seg["xml:id"] = p_seg_id
 
 f_output = open(f'../途中生成物/segID_{output_filename}', 'w', encoding='utf-8')
 f_output.write(str(soup))
