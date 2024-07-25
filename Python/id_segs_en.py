a756c6989149ef078ed1a474902a3c90b2888e01
naoki kokaze <33674696+naoki-kokaze@users.noreply.github.com>
2024-07-25 14:21:52 +0900

    Pythonファイルの整理

--- Python/id_segs_en.py
+++ Python/id_segs_en.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as BS
 vol = input('英訳の巻数は?\n')
 output_filename = f'engishiki_v{vol}_en.xml'
 
-f_input = open(output_filename, 'rb')
+f_input = open(f'../TEI編集用/{output_filename}', 'rb')
 soup = BS(f_input, 'xml')
 f_input.close()
 
@@ -11,10 +11,10 @@ paragraphs = soup.select('p[ana]')
 
 for p in paragraphs:
     segs = p.select('seg')
-    for index, anchor in enumerate(segs):
+    for index, seg in enumerate(segs):
         seg_id = 'footnote' + p["xml:id"][4:] + str(index + 1).zfill(2)
         seg["xml:id"] = seg_id
 
-f_output = open(output_filename, 'w', encoding='utf-8')
+f_output = open(f'../途中生成物/segID_{output_filename}', 'w', encoding='utf-8')
 f_output.write(str(soup))
 f_output.close()
