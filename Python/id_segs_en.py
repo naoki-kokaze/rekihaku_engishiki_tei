53e8ff1b85e1d3ec128761dc2d1a63e030fecffd
naoki kokaze <33674696+naoki-kokaze@users.noreply.github.com>
2024-08-07 14:14:03 +0900

    <article>内のすべての<p>タグを対象に英訳註IDを付与するよう改良

--- Python/id_segs_en.py
+++ Python/id_segs_en.py
@@ -10,19 +10,20 @@ f_input.close()
 articles = soup.select('div[type="条"]')
 
 for article in articles:
-    note_tag = article.find('note')
-    p_tag = article.find('p')    
-    
-    note_segs = note_tag.select('seg')
-    p_segs = p_tag.select('seg')
+    note_tag = article.find('note', {'type': 'summary'})
+    p_tags = article.find_all('p')
 
-    for index, note_seg in enumerate(note_segs):
-        note_seg_id = 'footnote' + article["xml:id"][7:] + '00' + str(index + 1).zfill(2)
-        note_seg["xml:id"] = note_seg_id
+    if note_tag:
+        note_segs = note_tag.select('seg')
+        for index, note_seg in enumerate(note_segs):
+            note_seg_id = 'footnote' + article["xml:id"][7:] + '00' + str(index + 1).zfill(2)
+            note_seg["xml:id"] = note_seg_id
 
-    for index, p_seg in enumerate(p_segs):
-        p_seg_id = 'footnote' + p_tag["xml:id"][4:] + str(index + 1).zfill(2)
-        p_seg["xml:id"] = p_seg_id
+    for p_tag in p_tags:
+        p_segs = p_tag.select('seg')
+        for index, p_seg in enumerate(p_segs):
+            p_seg_id = 'footnote' + p_tag["xml:id"][4:] + str(index + 1).zfill(2)
+            p_seg["xml:id"] = p_seg_id
 
 f_output = open(f'../途中生成物/segID_{output_filename}', 'w', encoding='utf-8')
 f_output.write(str(soup))
